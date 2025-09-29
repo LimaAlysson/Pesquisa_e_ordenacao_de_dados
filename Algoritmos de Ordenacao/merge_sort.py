766f8d4aca0f9c889456dbd3d40b31086c1e340f
LimaAlysson <alyssonredes32@gmail.com>
2025-09-29 14:36:17 -0300

    Feat/Implementando merge_sort

---
+++ Algoritmos de Ordenacao/merge_sort.py
@@ -0,0 +1,31 @@
+
+def merge_sort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+
+    if(fim - inicio > 1):
+        meio = (fim + inicio) // 2
+        merge_sort(lista, inicio, meio)
+        merge_sort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+    
+    return lista
+
+def merge(lista, inicio, meio, fim):
+    esquerda = lista[inicio:meio]
+    direita = lista[meio:fim]
+    menor_esquerda, menor_direita = 0, 0
+
+    for i in range(inicio, fim):
+        if menor_esquerda >= len(esquerda):
+            lista[i] = direita[menor_direita]
+            menor_direita += 1
+        elif menor_direita >= len(direita):
+            lista[i] = esquerda[menor_esquerda]
+            menor_esquerda += 1
+        elif esquerda[menor_esquerda] < direita[menor_direita]:
+            lista[i] = esquerda[menor_esquerda]
+            menor_esquerda += 1
+        else:
+            lista[i] = direita[menor_direita]
+            menor_direita += 1
